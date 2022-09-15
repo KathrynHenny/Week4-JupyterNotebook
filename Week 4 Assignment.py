0849155a256a3dec74c69da1d233c7636d5f9d25
KathrynHenny <112740636+KathrynHenny@users.noreply.github.com>
2022-09-15 13:29:03 -0500

    Jupyter Notebook

---
+++ Week 4 Assignment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Kathryn Hendrickson** 
+
+# I'm seeking a position working in the Data Science field.
+
+# *I am interested in research and using large scale data research to inform policy and standard operating procedures in mass transit.*
+
+# ### Print song lyrics from "Never Learn" by The Devil Makes Three
+
+# In[8]:
+
+
+print('Now just hear them talkin tounges child, Though you know not what they mean, Aint gonna make no kind of difference, Just keep your eyes focused on the screen.')
+
+
+# In[ ]:
+
+
+
+
